0d7ad5a539acbfff928b7c18f8c7df1fa86e5628
Abneribeiro <eliasabner38@gmail.com>
2023-08-08 15:43:31 +0000

    adicionando middlewares para formatar o username

--- src/models/User.js
+++ src/models/User.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-
+  this.username = this.username.toLowerCase();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,20 +1,38 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const user = new User({ username, email, password });
-    await user.save();
-    res.status(201).json({ message: "User registered successfully." });
-  } catch (error) {
-    res.status(500).json({ message: "Failed to register user." });
-    console.log(error);
+router.post(
+  "/register",
+  [
+    body("username")
+      .isLength({ min: 5 })
+      .withMessage("Username must be at least 5 characters long."),
+    body("email").isEmail().withMessage("Invalid email address."),
+    body("password").isStrongPassword().withMessage("Password must be strong."),
+  ],
+  async (req, res) => {
+    try {
+
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { username, email, password } = req.body;
+      const user = new User({ username, email, password });
+      
+      await user.save();
+
+      res.status(201).json({ message: "User registered successfully." });
+    } catch (error) {
+      res.status(500).json({ message: "Failed to register user." });
+      console.log(error);
+    }
   }
-});
+);
 
 router.post("/login", async (req, res) => {
   try {
